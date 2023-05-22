1196dfe686961bef0b3d3de701033ad0d4810efc
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-05-22 13:50:35 -0300

    adjust element order in display

--- src/observer/WeatherStation.java
+++ src/observer/WeatherStation.java
@@ -4,9 +4,9 @@ public class WeatherStation {
   public static void main(String[] args) {
     WeatherData weatherData = new WeatherData();
 
+    CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
     ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
     HeatIndexDisplay heatDisplay = new HeatIndexDisplay(weatherData);
-    CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
 
     weatherData.setMeasurements(80, 65, 30.4f);
     weatherData.setMeasurements(82, 70, 29.2f);
